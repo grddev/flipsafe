2f0e8817419f1ec28d301e285d67e571ddcca697
Gustav Munkby <grddev@gmail.com>
2011-09-26 12:11:32 +0000

    extend injection tests

--- library/test/inject.cc
+++ library/test/inject.cc
@@ -1,5 +1,8 @@
+#include <setjmp.h>
 #include <climits>
 #include <iostream>
+#include "data.hh"
+#include "handler.hh"
 
 static int global_counter;
 
@@ -19,8 +22,10 @@ struct faulty
     {
       const int bitcount = CHAR_BIT * sizeof(T);
       if (global_counter < bitcount)
-        ((char *)&value)[global_counter / CHAR_BIT] ^= (1 << (global_counter % CHAR_BIT));
-
+      {
+        // std::cout << "Inject" << std::endl;
+        ((char *)(T*)&value)[global_counter / CHAR_BIT] ^= (1 << (global_counter % CHAR_BIT));
+      }
       global_counter -= bitcount;
     }
   }
@@ -36,24 +41,72 @@ struct faulty
 
 // TODO: Compound assignment operators
 
+template <typename T>
 int under_test(int input)
 {
-  faulty<float> x = input;
+  faulty<T> x = (T)(input);
   if (x <= 2)
     x = x + 1;
   return x;
 }
 
-int main()
+jmp_buf retbuf;
+void throw_on_detected()
 {
+  longjmp(retbuf, 1);
+}
+
+template <typename T>
+void test_case()
+{
+  global_counter = -1;
+  int input = 1;
+  int expected = under_test<T>(input);
+
+  sihft::set_fault_detected(throw_on_detected);
+  int impact_free = 0, corrupted = 0, crashed = 0;
   int start = 0;
-  do {
+  while (true) {
     global_counter = start++;
-    std::cout << "Result " << under_test(1) << " expected " << 2 << std::endl;
+
+    if (!setjmp(retbuf))
+    {
+      int result = under_test<T>(input);
+      if (global_counter >= 0)
+        break;
+
+      if (result == expected)
+        impact_free++;
+      else
+      {
+        // std::cout << "Experiment " << start << " expected " << expected << " result " << result << std::endl;
+        corrupted++;
+      }
+    }
+    else {
+      crashed++;
+    }
   }
   while (global_counter < 0);
 
-  // Last injection is fault free
-  std::cout << "Injected " << (start - 1) << " faults" << std::endl;
+  std::cout <<
+    (start - 1) << ';' <<
+    crashed << ';' <<
+    impact_free << ';' <<
+    corrupted << std::endl;
 
+}
+
+#define TEST_CASE(type) do { std::cout << #type ";"; test_case<type>(); } while (0);
+
+int main()
+{
+  std::cout << "Type;Injected;Detected;Impactless;Corrupted\n";
+  TEST_CASE(int);
+  TEST_CASE(float);
+  TEST_CASE(sihft::dup<int>);
+  TEST_CASE(sihft::dup<float>);
+  TEST_CASE(sihft::tri<int>);
+  TEST_CASE(sihft::tri<float>);
+  TEST_CASE(sihft::trump<int>);
 }
