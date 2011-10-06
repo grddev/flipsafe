56ac18c93327687951cfea10459023205100a05b
Gustav Munkby <grddev@gmail.com>
2011-10-06 15:40:11 +0000

    support different directory depths in does.sh

--- does.sh
+++ does.sh
@@ -1,8 +1,14 @@
-redo-ifchange ../does.sh
+# Get root dir... assumes that the file was sourced from a
+# script inside the source tree (this file should be in root)
+root=$(readlink -f ${BASH_SOURCE:-$0})
+while [ \! -f "$root/does.sh" ]; do
+  root=${root%/*}
+done
+redo-ifchange "$root/does.sh"
 
 function c++() {
   depfile=/tmp/does.dep.$$
-  g++ -I../include -I. -Wall -Werror -Wextra -MT x -MD -MF $depfile "$@"
+  g++ -I"$root/include" -I. -Wall -Werror -Wextra -MT x -MD -MF $depfile "$@"
   result=$?
   sed '1s,.*: *,,;s,\\$,,' < $depfile | xargs redo-ifchange
   rm -- $depfile 2> /dev/null
